b2cd4c56d5d597ef4b97eeec03ba081fe23f5e9e
nemzvu <sinikmisha7@gmail.com>
2025-02-25 11:42:39 +0100

    CheckpointSystem 0.1
    made a Checkpoint object. created 2 scripts that dont work yet. RespawnScript is currently in development

---
+++ Assets/Scripts/RespawnScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnScript : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject respawnPoint;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = new Vector3(0, 0, -5);
+            //player.transform.position = respawnPoint.transform.position;
+            Debug.Log("Player Detected");
+        }
+    }
+}
